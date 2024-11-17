35b575b4c86bb707e3ff9bd46e57d9e9be0e4135
Kir <pelmen1109@gmail.com>
2024-11-17 19:53:25 +0200

    complete ui and validation for small nums

---
+++ cmake-build-debug/CMakeFiles/KP_7.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KP_7.dir/main.c.obj"
+  "CMakeFiles/KP_7.dir/main.c.obj.d"
+  "KP_7.exe"
+  "KP_7.exe.manifest"
+  "KP_7.pdb"
+  "libKP_7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/KP_7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
